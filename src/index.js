88ff0fb1db1e07aa4cdf93275c7d048569a1b353
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-03-10 19:17:33 -0300

    feat: not create account cpf exists

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const findCostumerCpfExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (findCostumerCpfExists) {
+    return response.status(400).json({ error: `Customer already exists with CPF: ${cpf}!` });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  console.log(customers);
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
