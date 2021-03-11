c6a78e8c7a088da48d08848c06f5791d1a9e814b
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-03-11 09:29:06 -0300

    feat: add middleware find customer by cpf

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Costumer not found!' });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -28,15 +42,8 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Costumer not found!' });
-  }
-
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.status(200).json(customer.statement);
 });
 
